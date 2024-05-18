98c3692cdc35a3fa2b8359ce1f6ac9239d202a37
MISTRXPC\ludek <ludek.p@email.cz>
2024-05-18 23:00:09 +0200

    Fix issue with engine for sql alchemy

--- database.py
+++ database.py
@@ -1,9 +1,28 @@
-from sqlalchemy import create_engine
+import logging
+
+from sqlalchemy import create_engine, Connection, Engine
 from config import config
 
 
 class Database:
 
     def __init__(self):
+        self._engine: Engine = self._create_engine()
+        self._connection: Connection = self._engine.connect().execution_options(stream_results=True)
+
+    def __del__(self):
+        self._disconnect()
+
+    def _disconnect(self):
+        self._connection.close()
+
+    @staticmethod
+    def _create_engine() -> Engine:
         url: str = f"postgresql+psycopg2://{config.database_username}:{config.database_password}@{config.database_host}:{config.database_port}/{config.database_database}"
-        self.database_engine = create_engine(url=url)
+        return create_engine(url=url)
+
+    def get_engine(self) -> Engine:
+        return self._engine
+
+    def get_connection(self) -> Connection:
+        return self._connection
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+from sqlalchemy import text
 from config import config
 from database import Database
 
@@ -7,7 +9,15 @@ print(f"database port: {config.database_username}")
 print(f"database port: {config.database_password}")
 print(f"database port: {config.database_database}")
 
+# Initiate logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(threadName)s] [%(levelname)s]  %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+
 db: Database = Database()
-with db.database_engine.connect() as connection:
-    result = connection.execute(text("select current_timestamp "))
-    print(f"database host: {config.database_host}")
+result = db.get_connection().execute(text("select * from t_order "))
+print(f"result: {result.fetchall()}")
+
